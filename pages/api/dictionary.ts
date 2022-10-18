05f2304d86f4c2abfe5b3069d9dc15d14a0768c8
alevann <55453451+alevann@users.noreply.github.com>
2022-10-18 15:33:33 +0200

    fix: yet another futile attempt

--- pages/api/dictionary.ts
+++ pages/api/dictionary.ts
@@ -1,39 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import path from 'path'
-import { promises as fs } from 'fs'
 
-export const config = {
-  api: {
-    responseLimit: false,
-  },
-}
-
-/**
- * A list of all supported languages
- */
-type Language = 'en'
+const AvailableLanguages = [
+  'en'
+]
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Dictionary>
 ) {
-  req.query.lang = req.query.lang ?? 'en'
-  const dict = await getDictionaryFromLanguage(req.query.lang as Language)
+  const lang = (req.query.lang ?? 'en') as string
 
-  if (dict) {
-    res.status(200).json(dict)
+  if (AvailableLanguages.includes(lang)) {
+    res.redirect(`/${lang}.json`)
   } else {
     res.status(404)
   }
-}
-
-const cache: { [k in Language]?: Dictionary } = {}
-
-async function getDictionaryFromLanguage (lang: Language): Promise<Optional<Dictionary>> {
-  if (!cache[lang]) {
-    const file = path.join(process.cwd(), 'assets', `${lang}.json`)
-    cache[lang] = await fs.readFile(file, 'utf-8') as unknown as Dictionary
-  }
-  return cache[lang]
 }
